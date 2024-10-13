ebd4399d476a75ad6eca615dd3bd449cff2b4943
hakaiyued <hakaiyued@gmail.com>
2024-10-14 07:48:48 +0800

    RL_simple_debug

--- test_sample1.py
+++ test_sample1.py
@@ -2,9 +2,28 @@ import gymnasium as gym
 
 from stable_baselines3 import A2C
 
-env = gym.make("CartPole-v1", render_mode="rgb_array")
+import numpy as np
 
-model = A2C("MlpPolicy", env, verbose=1)
+from gymnasium.spaces import Discrete
+class DiscreteActions(gym.ActionWrapper):
+    def __init__(self, env, disc_to_cont):
+        super().__init__(env)
+        self.disc_to_cont = disc_to_cont
+        self.action_space = Discrete(len(disc_to_cont))
+
+    def action(self, act):
+        return self.disc_to_cont[act]
+
+
+
+# env = gym.make("CartPole-v1", render_mode="rgb_array")
+env = gym.make("TB-v1", render_mode="human")
+# env = gym.make("Reacher-v4", render_mode="human")
+wrapped_env = DiscreteActions(env, [np.array([1,0]), np.array([-1,0]),
+                                        np.array([0,1]), np.array([0,-1])])
+
+# model = A2C("MlpPolicy", env, verbose=1)
+model = A2C("MlpPolicy", wrapped_env, verbose=1)
 model.learn(total_timesteps=10_000)
 
 vec_env = model.get_env()
